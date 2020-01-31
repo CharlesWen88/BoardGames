4cc0b91a1dd088c374f5e336c99191ba7bd52cfd
Amanda Jia Xuan Chua <e0390026@u.nus.edu>
2020-01-31 18:08:30 +0800

    HERE

--- main.js
+++ main.js
@@ -9,13 +9,20 @@ const PORT = parseInt(process.argv[2] || process.env.APP_PORT) || 3000;
 // Load configuration 
 const config = require('./config.json');
 //const URL = config.mongo || 'mongodb://localhost:27017';
-const URL = "mongodb://wilma:123@hydra-shard-00-00-ppljg.gcp.mongodb.net:27017,hydra-shard-00-01-ppljg.gcp.mongodb.net:27017,hydra-shard-00-02-ppljg.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Hydra-shard-0&authSource=admin&retryWrites=true&w=majority";
+//const URL = "mongodb://wilma:123@hydra-shard-00-00-ppljg.gcp.mongodb.net:27017,hydra-shard-00-01-ppljg.gcp.mongodb.net:27017,hydra-shard-00-02-ppljg.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Hydra-shard-0&authSource=admin&retryWrites=true&w=majority";
+const URL = "mongodb+srv://wilma:wilma@hydra-u1vel.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 // Create an instance of MongoClient
 const client = new MongoClient(URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //Create an instance of the application
 const app = express();
+
+var bodyParser = require('body-parser');
+// urlencoded tells body-parser to extract data from <form> element 
+// add them to the body property in the request object 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //GET /api/boardgames/name/:name
 //db.getCollection('games').find({name: ""})
@@ -123,7 +130,8 @@ app.get('/api/comments/:id', (req, res) => {
 
     client.db('boardgame')
         .collection('comments')
-        .find({ ID: id })
+        .find({ID:id,comment:{$ne:''}})
+        .sort({_id: -1})
         //.project({ Name: 1, ID: 1 })
         .limit(10)
         .toArray()
@@ -141,6 +149,28 @@ app.get('/api/comments/:id', (req, res) => {
             res.status(404)
             res.json({ message: error });
         })
+    }
+)
+
+//post comments
+app.post('/api/comments', (req, res) => {
+    console.info(req.body);
+    client.db('boardgame').collection('comments').insertOne(
+        {   "user": req.body.user,
+            "rating":parseInt(req.body.rating),
+            "comment": req.body.comment,
+            "ID": parseInt(req.body.gameid),
+            "name": req.body.name 
+        },
+            
+         (err,result)=>{
+        if (err) 
+            console.log(err);
+        else
+            console.log('saved to database');
+            console.info('saved');
+    })
+    res.redirect('back');
     }
 )
 
